0ebba706f52f60e017a48a0d7852c0bb56442f63
galiov-w <galilea.valle@wizeline.com>
2022-01-31 21:39:56 -0600

    feat: add notification for secret manager versions

--- index.js
+++ index.js
@@ -6,20 +6,84 @@ const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
 
 exports.subscribe = pubsubMessage => {
-    // Print out the data from Pub/Sub, to prove that it worked
-    const build = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString());
-    if (build.status === 'WORKING' || build.status === 'SUCCESS' || build.status === 'FAILURE' || build.status === 'CANCELLED') {
-        console.log(`build info ${JSON.stringify(build)}`);
-        const message = generateSlackMessage(build);
-        (async () => {
-            try {
-                await webhook.send(message);
-            } catch (err) {
-                console.log(err); // TypeError: failed to fetch
-            }
-        })();
+    if (pubsubMessage.attributes.buildId){
+        // Print out the data from Pub/Sub, to prove that it worked
+        const build = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString());
+        if (build.status === 'WORKING' || build.status === 'SUCCESS' || build.status === 'FAILURE' || build.status === 'CANCELLED') {
+            console.log(`build info ${JSON.stringify(build)}`);
+            const message = generateSlackMessage(build);
+            (async () => {
+                try {
+                    await webhook.send(message);
+                } catch (err) {
+                    console.log(err); // TypeError: failed to fetch
+                }
+            })();
+        } else {
+            console.log(`build unknown status type ${JSON.stringify(build)}`);
+        }
+    } else if (pubsubMessage.attributes.eventType) {
+        const event_type = pubsubMessage.attributes.eventType;
+        const secretId = pubsubMessage.attributes.secretId.slice(30);
+        const secretMetadata = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString());
+        if (event_type === 'SECRET_VERSION_ADD' || event_type === "SECRET_VERSION_DESTROY") {
+            console.log(`Event type ${event_type}. Metadata: ${JSON.stringify(secretMetadata)}`);
+            const message = generateSecretSlackMessage(secretId, secretMetadata);
+            (async () => {
+                try {
+                    await webhook.send(message);
+                } catch (err) {
+                    console.log(err); // TypeError: failed to fetch
+                }
+            })();
+        } else {
+            console.log(`secret unknown event type ${event_type}. Metadata: ${JSON.stringify(secretMetadata)}`);
+        }
+    }
+}
+
+const generateSecretSlackMessage = (secretId, secretMetadata) => {
+    let version = secretMetadata.name.split("/")[5];
+    let state = secretMetadata.state.toLowerCase();
+
+    // add status emoji
+    let msg = ':shushing_face:'
+
+    msg += ` Secret - `
+    // add environment name
+    if (secretMetadata.name.includes("363532665815")) {
+        msg += "DEVELOPMENT"
+    } else if (secretMetadata.name.includes("671385210864")) {
+        msg += "QA2"
+    } else if (secretMetadata.name.includes("997717717493")) {
+        msg += "QA1"
+    } else if (secretMetadata.name.includes("834589263806")) {
+        msg += "STAGE"
     } else {
-        console.log(`build unknown status type ${JSON.stringify(build)}`);
+        msg += "PRODUCTION"
+    }
+
+    let actionMsg
+    switch (state) {
+        case 'destroyed':
+            actionMsg = `Version ${version} destroyed`
+            break;
+        case 'enabled':
+            actionMsg = `New version ${version} added`
+            break;
+    }
+
+    return {
+        mrkdwn: true,
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `${msg}\n       *${secretId}* - ${actionMsg}`,
+                },
+            }
+        ]
     }
 }
 
@@ -63,7 +127,7 @@ const generateSlackMessage = (build) => {
     } else if (projectId.includes("qa2")) {
         msg += "QA2"
     } else if (projectId.includes("qa")) {
-        msg += "QA" + (triggerName.includes("-2") ? "2" : "1" ) // requested by luca
+        msg += "QA1"
     } else if (projectId.includes("stage")) {
         msg += "STAGE"
     } else {
